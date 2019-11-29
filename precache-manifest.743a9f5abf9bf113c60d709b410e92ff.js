5252dc58fd45f3342badc0c6ac18d380597f543c
Neelesh Bisht <neeleshbisht99@gmail.com>
2019-11-29 19:16:14 +0530

    Updates

---
+++ precache-manifest.743a9f5abf9bf113c60d709b410e92ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d3e1687f4cf94e799dba9699f6f7d2",
+    "url": "/interview-test/index.html"
+  },
+  {
+    "revision": "f61d452ba4984ea15754",
+    "url": "/interview-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "df500e36efd9033d8925",
+    "url": "/interview-test/static/js/2.4f625185.chunk.js"
+  },
+  {
+    "revision": "f61d452ba4984ea15754",
+    "url": "/interview-test/static/js/main.b945a7fb.chunk.js"
+  },
+  {
+    "revision": "f29ff8b851dd34e120da",
+    "url": "/interview-test/static/js/runtime-main.047b2b12.js"
+  }
+]);
